8201612ccd93d704d919ea8980066fad84c41e6b
robotescu <80849623+robotescu@users.noreply.github.com>
2022-01-24 17:50:23 +0200

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 basic.show_string("Apasa A")
 def on_button_pressed_a():
     numerele_castigatoare = []        
+    # Parcurgem lista
     for i in range (0,6):
-        numar = randint(1, 49)
+        numar = randint(1, 49) # generam un numar intre 1 si 49
+        # Verificam daca numarul generat exista deja in numerele_castigatoare
         while numar in numerele_castigatoare:
-            numar = randint(1, 49)
+            numar = randint(1, 49) # daca numarul exista in lista, genereaza unul nou
+        # Iesim din while cand numarul generat nu e in lista
         numerele_castigatoare.push(numar)  
     basic.show_string("Castigatorare:")
+    # Parcurg lista si afisez fiecare numar
     for i in range(0,6):
         basic.pause(500)
         basic.show_number(numerele_castigatoare[i])
--- main.ts
+++ main.ts
@@ -3,14 +3,20 @@ input.onButtonPressed(Button.A, function on_button_pressed_a() {
     let i: number;
     let numar: number;
     let numerele_castigatoare = []
+    //  Parcurgem lista
     for (i = 0; i < 6; i++) {
         numar = randint(1, 49)
+        //  generam un numar intre 1 si 49
+        //  Verificam daca numarul generat exista deja in numerele_castigatoare
         while (numerele_castigatoare.indexOf(numar) >= 0) {
             numar = randint(1, 49)
         }
+        //  daca numarul exista in lista, genereaza unul nou
+        //  Iesim din while cand numarul generat nu e in lista
         numerele_castigatoare.push(numar)
     }
     basic.showString("Castigatorare:")
+    //  Parcurg lista si afisez fiecare numar
     for (i = 0; i < 6; i++) {
         basic.pause(500)
         basic.showNumber(numerele_castigatoare[i])
